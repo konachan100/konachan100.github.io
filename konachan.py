6915802ba32d3c8689bbdf87bc39bdffc28e8677
YSPersonal <sniperhys@126.com>
2018-06-01 03:21:57 +0800

    add files

---
+++ konachan.py
@@ -0,0 +1,37 @@
+import json
+import threading
+import urllib
+import urllib.request
+
+import time
+
+postlist = []
+
+
+def webread(url, readtimeout=10):
+    """read page"""
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent',
+                          'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'),
+                         ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')]
+    return opener.open(url, None, readtimeout).read()
+
+
+def update():
+    global postlist
+    postlist = json.loads(webread('http://www.konachan.net/post.json?limit=100').decode())
+    for p in postlist:
+        #print(p)
+        p['file_url'] = p['file_url'].replace('konachan.net', 'konachan.com')
+        p['jpeg_url'] = p['jpeg_url'].replace('konachan.net', 'konachan.com')
+
+
+def update_loop():
+    while True:
+        update()
+        time.sleep(300)
+
+def start_update_thread():
+    thread=threading.Thread(target=update_loop)
+    thread.setDaemon(True)
+    thread.start()
---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import konachan
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    row=[]
+    col=[]
+    for i in range(len(konachan.postlist)):
+        col.append(konachan.postlist[i])
+        if (i+1)%4==0:
+            row.append(col)
+            col=[]
+
+    if len(col)>0:
+        row.append(col)
+
+    return render_template('postlist.html', postrow=row)
+
+
+if __name__ == '__main__':
+    konachan.start_update_thread()
+    app.run(port=80)
+
+
+
