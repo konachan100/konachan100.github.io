b81de36509f8b6975c92fb82cd2abdde6b46b6d4
konachan100 <konachan100@163.com>
2020-03-02 11:29:26 +0800

    update content

--- trimlog.py
+++ trimlog.py
@@ -3,7 +3,7 @@ with open('log.txt', 'r') as f:
     lines = f.readlines()
 
 maxline = 500
-if len(lines>maxline):
+if len(lines)>maxline:
     lines = lines[-500:]
 with open('log.txt', 'w') as f:
     f.writelines(lines)
